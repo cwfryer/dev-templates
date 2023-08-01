881263f2b6b0ae71cbfa979abae98cbdd79cc24f
Casey Fryer <caseyfryer@gmail.com>
2023-08-01 10:49:04 -0700

    added comments about fenix toolchain

--- rust/flake.nix
+++ rust/flake.nix
@@ -19,7 +19,18 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
+        # you either want `stable` or `latest` (aka nightly)
         toolchain = fenix.packages."${system}".stable.toolchain;
+        # <whatever>.toolchain will give you all the tools.
+        # You can define more granularly (e.g. to use alternate targets)
+        # by using combine like below:
+        # toolchain = with fenix.packages."${system}";
+        #   combine [
+        #     latest.cargo
+        #     latest.rustc
+        #     targets.wasm32-unknown-unknown.latest.rust-std
+        #   ];
+        # See https://github.com/nix-community/fenix documentation
       in rec {
         devShell = pkgs.mkShell {
           nativeBuildInputs = [
