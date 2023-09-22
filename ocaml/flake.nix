72b9ef05fcadd8df94b2ea9223dc69e116d4ca12
Casey Fryer <caseyfryer@gmail.com>
2023-09-22 10:36:18 -0700

    updated ocaml flake to use opam-nix

--- ocaml/flake.nix
+++ ocaml/flake.nix
@@ -1,21 +1,49 @@
 {
   description = "A Nix-flake-based OCaml development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ ocaml ocamlformat ] ++
-            (with pkgs.ocamlPackages; [ dune_3 odoc ]);
-        };
-      });
-    };
+  outputs = {
+    self,
+    opam-nix,
+    nixpkgs,
+    flake-utils,
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      ocamlPackages = pkgs.ocamlPackages;
+      on = opam-nix.lib.${system};
+
+      # Put your project dependencies here.
+      # "package" = "version"; where "package" = "*" chooses latest.
+      devPackagesQuery = {
+      };
+      query =
+        devPackagesQuery
+        # If you need a certain ocaml version, put "ocaml-base-compiler" = "version" in here.
+        // {};
+      scope = on.buildOpamProject' {inherit pkgs;} ./. query;
+      # Use this overlay to make changes to your packages
+      overlay = final: prev: {
+        # Example:
+        # package = prev.package.overrideAttrs (_: {
+        #   buildPhase = "dune build --release"
+        #   See https://github.com/tweag/opam-nix/blob/main/DOCUMENTATION.md#package for more info
+        # })
+      };
+      scope' = scope.overrideScope' overlay;
+      devPackages =
+        builtins.attrValues
+        (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = devPackages;
+        # Add any additional packages that you need here (e.g. ocamlformat)
+        nativeBuildInputs = [];
+      };
+    });
 }
